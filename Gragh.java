e5d0390d623d5e947fa40527184036ffc0283b20
dxquyet16 <quyetcoder16@gmail.com>
2024-10-30 15:39:09 +0700

    done template

---
+++ Gragh.java
@@ -0,0 +1,195 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+void dijkstra(int src, int dest, RandomAccessFile f) throws Exception {
+    int[] dist = new int[n];
+    boolean[] visited = new boolean[n];
+    int[] parent = new int[n];
+    int INF = 99;
+    Arrays.fill(dist, INF);
+    Arrays.fill(visited, false);
+    Arrays.fill(parent, -1);
+    dist[src] = 0;
+    parent[src] = -1;
+
+    PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(arr -> arr[1]));
+    pq.add(new int[] { src, 0 });
+    while (!pq.isEmpty()) {
+        int u = pq.poll()[0];
+
+        if (visited[u]) {
+            continue;
+        }
+
+        visited[u] = true;
+
+        if (dist[u] == INF) {
+            continue;
+        }
+        for (int v = 0; v < n; v++) {
+            if (!visited[v]
+                    && a[u][v] != 0
+                    && dist[v] > dist[u] + a[u][v]) {
+                dist[v] = dist[u] + a[u][v];
+                parent[v] = u;
+                pq.add(new int[] { v, dist[v] });
+            }
+        }
+    }
+
+    int count = 0;
+    int[] path = new int[n];
+    int tmp = dest;
+    while (tmp != -1) {
+        path[count] = tmp;
+        tmp = parent[tmp];
+        count++;
+    }
+
+    for (int i = count - 1; i >= 0; i--) {
+        fvisit(path[i], f);
+    }
+    f.writeBytes("\n");
+    for (int i = count - 1; i >= 0; i--) {
+        f.writeBytes(" " + dist[path[i]]);
+    }
+}
+
+// -- tim eulerCycle
+
+void findEulerianCycle(int start) {
+    if (!hasEulerianCycle()) {
+        System.out.println("Đồ thị không có chu trình Euler.");
+        return;
+    }
+    Stack<Integer> stack = new Stack<>();
+    ArrayList<Integer> cycle = new ArrayList<>();
+    stack.push(start);
+    while (!stack.isEmpty()) {
+        int u = stack.peek();
+        int i;
+        for (i = 0; i < n; i++) {
+            if (a[u][i] != 0) {
+                stack.push(i);
+                a[u][i] = a[i][u] = 0;
+                break;
+            }
+        }
+        if (i == n) {
+            cycle.add(stack.pop());
+        }
+    }
+    System.out.print("Chu trình Euler: ");
+    for (int v : cycle) {
+        System.out.print(v + " ");
+    }
+}
+
+// euler cycle
+boolean hasIsolated() {
+    for (int i = 0; i < n; i++) {
+        if (deg(i) == 0) {
+            return (true);
+        }
+    }
+    return (false);
+}
+
+boolean isConnected() {
+    boolean[] visited = new boolean[n];
+    // Stack<Integer> stack = new Stack<>();
+    Stack stack = new Stack();
+    for (int i = 0; i < n; i++) {
+        visited[i] = false;
+    }
+    stack.push(0);
+    visited[0] = true;
+
+    while (!stack.isEmpty()) {
+        int current = stack.pop();
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && a[current][i] > 0) {
+                stack.push(i);
+                visited[i] = true;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// kiem tra do thi có vo huong khong
+boolean isUnDirected() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] != a[j][i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// kiem tra tat ca dinh co chan khong
+boolean allDegEven() {
+    for (int i = 0; i < n; i++) {
+        if (deg(i) % 2 == 1) {
+            return (false);
+        }
+    }
+    return (true);
+}
+
+// kiem tra co euler cycle khong
+boolean hasEulerCycle() {
+    if (!hasIsolated() && isUnDirected() && isConnected() && allDegEven()) {
+        return (true);
+    } else {
+        return (false);
+    }
+}
+
+void eulerCycle(int fro, RandomAccessFile f) throws IOException {
+    if (!hasEulerCycle()) {
+        return;
+    }
+
+    int[] eulerPath = new int[100];
+    int index = 0;
+
+    Stack stack = new Stack();
+    stack.push(fro);
+
+    while (!stack.isEmpty()) {
+        int current = stack.top();
+        int i;
+
+        for (i = 0; i < n; i++) {
+            if (a[current][i] > 0) {
+                break;
+            }
+        }
+
+        if (i == n) {
+            stack.pop();
+            eulerPath[index++] = current;
+        } else {
+            stack.push(i);
+            a[current][i]--;
+            a[i][current]--;
+        }
+    }
+    for (int i = 0; i < index; i++) {
+        f.writeBytes(v[eulerPath[i]] + " ");
+    }
+}
---
+++ tree.java
@@ -0,0 +1,206 @@
+// them dung de quy
+Node insert(Node currentNode, Brick value) {
+    if (root == null) {
+        root = new Node(value);
+        return null;
+    }
+
+    if (currentNode == null) {
+        return new Node(value);
+    }
+    // if (value.type == currentNode.info.type) {
+    // return currentNode;
+    // }
+    if (value.type > currentNode.info.type) {
+        currentNode.right = insert(currentNode.right, value);
+    } else {
+        currentNode.left = insert(currentNode.left, value);
+    }
+
+    return currentNode;
+}
+
+// them khong dung de quy
+void insert(Ball newBall) {
+    Node newNode = new Node(newBall);
+
+    if (root == null) {
+        root = newNode;
+        return;
+    }
+
+    Node parentNode = null;
+    Node currentNode = root;
+
+    while (currentNode != null) {
+        if (currentNode.info.type == newBall.type) {
+            return;
+        }
+
+        parentNode = currentNode;
+
+        if (newBall.type < currentNode.info.type) {
+            currentNode = currentNode.left;
+        } else {
+            currentNode = currentNode.right;
+        }
+    }
+
+    if (newBall.type < parentNode.info.type) {
+        parentNode.left = newNode;
+    } else {
+        parentNode.right = newNode;
+    }
+}
+
+// xoa node
+Node rBST_Delete(Node currentNode, int value) {
+
+    if (currentNode == null) {
+        return null;
+    }
+    if (value == currentNode.info.price) {
+        if (currentNode.left == null) {
+            return currentNode.right;
+        } else {
+            if (currentNode.right == null) {
+                return currentNode.left;
+            }
+        }
+
+        // // find minValue in sub right tree
+        // Node minNode = findMin(currentNode.right);
+        // int minValue = minNode.info.price;
+        //// asign minvalue of sub right tree to value of currentNode
+        //
+        // currentNode.info = minNode.info;
+        //// delete node hava minValue in sub right tree
+        // currentNode.right = rBST_Delete(currentNode.right, minValue);
+        // return currentNode;
+        // find max in sub right tree
+        Node maxNode = findMax(currentNode.left);
+        int maxValue = maxNode.info.price;
+        // asign minvalue of sub right tree to value of currentNode
+
+        currentNode.info = maxNode.info;
+        // delete node hava minValue in sub right tree
+        currentNode.left = rBST_Delete(currentNode.left, maxValue);
+        return currentNode;
+    }
+    if (value > currentNode.info.price) {
+        currentNode.right = rBST_Delete(currentNode.right, value);
+    } else {
+        currentNode.left = rBST_Delete(currentNode.left, value);
+    }
+    return currentNode;
+}
+
+private Node findMin(Node currentNode) {
+    Node min = currentNode;
+    while (currentNode.left != null) {
+        currentNode = currentNode.left;
+        min.info = currentNode.info;
+    }
+    return min;
+}
+
+private Node findMax(Node currentNode) {
+    Node max = currentNode;
+    while (currentNode.right != null) {
+        currentNode = currentNode.right;
+        max.info = currentNode.info;
+    }
+    return max;
+}
+
+// quay phai
+
+Node findParent(Node child) {
+    if (child == root || child == null) {
+        return null;
+    }
+
+    Node currentNode = root;
+    Node parentNode = null;
+
+    while (currentNode != null) {
+        if (currentNode.info.price == child.info.price) {
+            break;
+        }
+        parentNode = currentNode;
+
+        if (currentNode.info.price > child.info.price) {
+            currentNode = currentNode.left;
+        } else {
+            currentNode = currentNode.right;
+        }
+    }
+
+    return parentNode;
+}
+
+void rotateRight(Node parent) {
+    if (parent == null || parent.left == null) {
+        return;
+    }
+
+    Node child = parent.left;
+    parent.left = child.right;
+    child.right = parent;
+
+    Node parentOfParent = findParent(parent);
+
+    if (parentOfParent == null) {
+        root = child;
+    } else if (parentOfParent.right == parent) {
+        parentOfParent.right = child;
+    } else {
+        parentOfParent.left = child;
+    }
+}
+
+// quay trái
+
+Node findParent(Node child) {
+    if (child == root || child == null) {
+        return null;
+    }
+
+    Node currentNode = root;
+    Node parentNode = null;
+
+    while (currentNode != null) {
+        if (currentNode.info.price == child.info.price) {
+            break;
+        }
+        parentNode = currentNode;
+
+        if (currentNode.info.price > child.info.price) {
+            currentNode = currentNode.left;
+        } else {
+            currentNode = currentNode.right;
+        }
+    }
+
+    return parentNode;
+}
+
+void rotateLeft(Node parent) {
+    if (parent == null || parent.right == null) {
+        return;
+    }
+
+    Node child = parent.right;
+    parent.right = child.left;
+    child.left = parent;
+
+    Node parentOfParent = findParent(parent);
+
+    if (parentOfParent == null) {
+        root = child;
+    } else if (parentOfParent.left == parent) {
+        parentOfParent.left = child;
+    } else {
+        parentOfParent.right = child;
+    }
+}
